97a98a60db32a0b1f20216ee68e8e353c20c8ce8
JB Creations <anuragrajput200274@gmail.com>
2025-09-22 16:34:59 +0530

    Add shebang to render scripts and make them executable

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # Render.com Build Script
 cd order-backend && cp package-minimal.json package.json && npm install
--- render-start.sh
+++ render-start.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # Render.com Start Command
 cd order-backend && node minimal-server.js
